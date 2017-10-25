4f113ce37b06286c8397de180fd70fce46ba97e3
Todd Atkins <toddatkins01@gmail.com>
2017-10-24 20:08:07 -0400

    starting point

---
+++ src/main/java/reviews/resturants/Restaurant.java
@@ -0,0 +1,24 @@
+package reviews.resturants;
+
+public class Restaurant {
+
+	private String name;
+	private String description;
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public Restaurant(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	
+	
+}
